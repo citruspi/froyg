b6fc89efb9db077e574a01c77106b2477d429098
Mihir Singh <git.service@mihirsingh.com>
2020-10-11 23:12:22 -0500

    Made tarball packaging script configurable via env

--- .ci/pkg-tarballs.sh
+++ .ci/pkg-tarballs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-rm -rf tarballs && mkdir tarballs
+rm -rf "$TAR_DIR" && mkdir "$TAR_DIR"
 rm -rf _build && mkdir _build
 
-for FILE in out/*; do
+for FILE in "$BIN_DIR/*"; do
   version=$(echo $FILE | awk -F '-' '{print $2,$3}' | xargs | tr ' ' '-')
 
   os=$(echo $version | awk -F '.' '{print $1}')
   arch=$(echo $version | awk -F '.' '{print $2}')
 
-  label="froyg-$CI_COMMIT_REF_SLUG-$os-$arch"
+  label="$NAME-$CI_COMMIT_REF_SLUG-$os-$arch"
 
   mkdir "_build/$label"
 
   cp LICENSE "_build/$label/LICENSE"
   cp $FILE "_build/$label/froyg"
 
-  cd _build && tar cfJ "../tarballs/$label.tar.xz" $label && echo "packaged $label.tar.xz"
+  cd _build && tar cfJ "../$TAR_DIR/$label.tar.xz" $label && echo "packaged $label.tar.xz"
   cd ..
 done
